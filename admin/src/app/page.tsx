06044ba2efad117eb3c96edc279973cd7f102370
trangnguyen <nguyenthitrang.ttd@gmail.com>
2025-04-24 23:44:26 +0700

    🎉 begin a admin project

---
+++ admin/src/app/page.tsx
@@ -0,0 +1,8 @@
+import MainLayout from "@src/components/layouts/main-layout";
+import UsersManagement from "@src/components/templates/_users";
+
+export default function Home() {
+  return (
+  <MainLayout main={<UsersManagement/>}/>
+  );
+}
